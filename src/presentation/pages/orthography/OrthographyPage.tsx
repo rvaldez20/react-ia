a641aa57c7ac5f4c56986e85e77177606ef13be3
rvaldez20 <rvaldez20@gmail.com>
2024-01-10 13:21:42 -0600

    [ADD] create function handlePost for show message Gpt or myMessage

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,6 +1,29 @@
+import { useState } from 'react'
 import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from '../../components'
 
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
 export const OrthographyPage = () => {
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  console.log(messages)
+
+  const handlePost = async( text: string ) => {
+    setIsLoading(true)
+    setMessages( (prev) => [...prev, { text: text, isGpt: false }] );
+
+    //TODO: use-case
+
+    setIsLoading(false)
+
+    // TODO: Añadir el message de isGPT en true
+  }
+
   return (
     <div className="chat-container">
       <div className="chat-messages">
@@ -9,18 +32,34 @@ export const OrthographyPage = () => {
           {/* Welcome GPT */}
           <GptMessage text='Hola, puedes escribir tu texto en español, y te ayudo con las correcciones' />
 
-          {/* User Messages  */}
-          <MyMessage text='Hola Mundo' /> 
+          {
+            messages.map( (message, index) => (
+              message.isGpt
+                ? (
+                    <GptMessage key={ index } text='Esto es de OpenIA' />
+                  )
+                : (
+                    <MyMessage key={ index } text={message.text} />
+                  )
+            ))
+          }
 
-          {/* loader that show that GPT will write some */}
-          <TypingLoader className="fade-in" />
+          {
+            isLoading && (
+              <div className='col-start-1 col-end-12 fade-in'>
+                {/* loader that show that GPT will write some */}
+                <TypingLoader />
+              </div>
+            )
+          }  
+          
 
         </div> {/* grid grid */}
       </div> {/* chat-messages */}
 
 
       <TextMessageBox 
-        onSendMessage={ message => console.log(message) }
+        onSendMessage={ handlePost }
         placeholder='Escribe aquí lo que deseas'
         disableCorrections
       />
